f071b66a105bb969bd0c259989000f95ec6232e0
sawicky <sawickd@protonmail.com>
2018-07-03 13:51:21 +1000

    migrate login page

--- index.php
+++ index.php
@@ -5,13 +5,24 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
+    <link rel = "stylesheet" type = "text/css" HREF = "style/Main.css">
     <head>
+        <title>Login</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <?php
-        echo date('l, F jS Y.');
-        ?>
+        
+            <form method="post" action="index.jsp">
+                <div class ="center">
+                    <table class="register">
+                        <tr><td><input class="button" name="user" placeholder="Enter username ..."></td></tr>
+                        <tr><td><input class="button" name="pass" type ="password" placeholder="Enter password"></td></tr>
+                        <input type="submit" name="update" value=" Apply " 
+                            style="position: absolute; height: 0px; width: 0px; border: none; padding: 0px;"
+                             hidefocus="true" tabindex="-1"/>
+                     </table>
+                </div>
+            </form>
     </body>
 </html>
